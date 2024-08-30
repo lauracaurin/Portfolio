b75b8e6b6615fe4793e534841a1247518d38b839
lauracaurin <laura_cd1@hotmail.com>
2024-08-30 22:25:02 +0100

    Experience component layout

--- src/components/pages/Experience.js
+++ src/components/pages/Experience.js
@@ -1,17 +1,115 @@
+import check from '../../images/checkmark.png';
+
 const Experience = () => {
   return (
     <>
-      <section id="experience">EXPXECIENCE<br></br>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo id quo at ut distinctio odit voluptatum assumenda iusto. Architecto dolor autem deleniti nam voluptas repellat optio sequi dignissimos aliquid animi
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo id quo at ut distinctio odit voluptatum assumenda iusto. Architecto dolor autem deleniti nam voluptas repellat optio sequi dignissimos aliquid animi
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo id quo at ut distinctio odit voluptatum assumenda iusto. Architecto dolor autem deleniti nam voluptas repellat optio sequi dignissimos aliquid animi
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo id quo at ut distinctio odit voluptatum assumenda iusto. Architecto dolor autem deleniti nam voluptas repellat optio sequi dignissimos aliquid animi
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo id quo at ut distinctio odit voluptatum assumenda iusto. Architecto dolor autem deleniti nam voluptas repellat optio sequi dignissimos aliquid animi
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo id quo at ut distinctio odit voluptatum assumenda iusto. Architecto dolor autem deleniti nam voluptas repellat optio sequi dignissimos aliquid animi
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo id quo at ut distinctio odit voluptatum assumenda iusto. Architecto dolor autem deleniti nam voluptas repellat optio sequi dignissimos aliquid animi
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo id quo at ut distinctio odit voluptatum assumenda iusto. Architecto dolor autem deleniti nam voluptas repellat optio sequi dignissimos aliquid animi
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo id quo at ut distinctio odit voluptatum assumenda iusto. Architecto dolor autem deleniti nam voluptas repellat optio sequi dignissimos aliquid animi
-        experience
+      <section className="section__experience" id="experience">
+
+        <h2 className="section__h2">Experiencia</h2>
+        <h3 className="section__h3">Habilidades técnicas</h3>
+
+        <div className="experience__container">
+          <div className="experience__details">
+            <h3 className="section__h3">Desarrollo Frontend</h3>
+            <div className="experience__items">
+              <div className="experience__item">
+                <img src={check} alt="Experience icon" className="icon" />
+                <div>
+                  <h3>HTML5</h3>
+                </div>
+              </div>
+              <div className="experience__item">
+                <img src={check} alt="Experience icon" className="icon" />
+                <div>
+                  <h3>CSS3</h3>
+                </div>
+              </div>
+              <div className="experience__item">
+                <img src={check} alt="Experience icon" className="icon" />
+                <div>
+                  <h3>Sass</h3>
+                </div>
+              </div>
+              <div className="experience__item">
+                <img src={check} alt="Experience icon" className="icon" />
+                <div>
+                  <h3>JavaScript</h3>
+                </div>
+              </div>
+              <div className="experience__item">
+                <img src={check} alt="Experience icon" className="icon" />
+                <div>
+                  <h3>React</h3>
+                </div>
+              </div>
+              <div className="experience__item">
+                <img src={check} alt="Experience icon" className="icon" />
+                <div>
+                  <h3>Material UI</h3>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="experience__details">
+            <h3 className="section__h3">Desarrollo Backend</h3>
+            <div className="experience__items">
+              <div className="experience__item">
+                <img src={check} alt="Experience icon" className="icon" />
+                <div>
+                  <h3>Node JS</h3>
+                </div>
+              </div>
+              <div className="experience__item">
+                <img src={check} alt="Experience icon" className="icon" />
+                <div>
+                  <h3>Express JS</h3>
+                </div>
+              </div>
+              <div className="experience__item">
+                <img src={check} alt="Experience icon" className="icon" />
+                <div>
+                  <h3>PostgreSQL</h3>
+                </div>
+              </div>
+              <div className="experience__item">
+                <img src={check} alt="Experience icon" className="icon" />
+                <div>
+                  <h3>Git</h3>
+                </div>
+              </div>
+              <div className="experience__item">
+                <img src={check} alt="Experience icon" className="icon" />
+                <div>
+                  <h3>HTML5</h3>
+                </div>
+              </div>
+              <div className="experience__item">
+                <img src={check} alt="Experience icon" className="icon" />
+                <div>
+                  <h3>HTML5</h3>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="experience__details">
+            <h3 className="section__h3">Metodologías y herramientas</h3>
+            <div className="experience__items">
+              <div className="experience__item">
+                <img src={check} alt="Experience icon" className="icon" />
+                <div>
+                  <h3>HTML5</h3>
+                </div>
+              </div>
+              <div className="experience__item">
+                <img src={check} alt="Experience icon" className="icon" />
+                <div>
+                  <h3>HTML5</h3>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
       </section>
     </>
   )
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,6 +1,6 @@
 const Projects = () => {
   return (<>
-    <section id="projects">PROJECTS <br></br>
+    <section className="section section__projects" id="projects">PROJECTS <br></br>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo id quo at ut distinctio odit voluptatum assumenda iusto. Architecto dolor autem deleniti nam voluptas repellat optio sequi dignissimos aliquid animi
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo id quo at ut distinctio odit voluptatum assumenda iusto. Architecto dolor autem deleniti nam voluptas repellat optio sequi dignissimos aliquid animi
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo id quo at ut distinctio odit voluptatum assumenda iusto. Architecto dolor autem deleniti nam voluptas repellat optio sequi dignissimos aliquid animi
